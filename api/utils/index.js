44f9fb766d84af7fa27b15b17a4f586b2e49481d
beelaw <lawrence.mcbhee@gmail.com>
2019-02-08 20:28:00 +0100

    Removes unused utils

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,10 +1,6 @@
-const mailer = require('./mailer');
 const errors = require('./errors');
-const readFile = require('./file_reader');
 
 
 module.exports = {
-  ...mailer,
   ...errors,
-  readFile,
 };
