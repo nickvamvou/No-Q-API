af674cffa1dcd5c06aab4907635dc97b30c7ebd8
vamvou <nik.vamvou@gmail.com>
2019-03-20 16:07:55 +0400

    added cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const productDetailsRouter = require("./api/product_details/index");
 var storeRouter = require("./api/store/index");
 var usersRouter = require("./api/user/index");
 var cartRouter = require("./api/shopping_cart/index");
-const supportAPI = require('./api/support');
+const supportAPI = require("./api/support");
 
 var app = express();
 
@@ -33,17 +33,17 @@ app.use(
     extended: false
   })
 );
-// //adding headers to every response. From where we can receive requests
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   //allow all headers
-//   res.header("Access-Control-Allow-Headers", "*");
-//   //this is the first request that the broswer sends to check if it can make a request
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//     return res.status(200).json({});
-//   }
-// });
+//adding headers to every response. From where we can receive requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  //allow all headers
+  res.header("Access-Control-Allow-Headers", "*");
+  //this is the first request that the broswer sends to check if it can make a request
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+});
 
 /*
 *******************************************************************************
@@ -51,7 +51,7 @@ app.use(
 *******************************************************************************
 */
 app.use("/", indexRouter);
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 app.use("/user", usersRouter);
 app.use("/products", productsRouter);
 app.use("/productDetails", productDetailsRouter);
