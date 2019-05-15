e64e2916d4138891e50bf33c8d7d7451e6e23a49
beelaw <lawrence.mcbhee@gmail.com>
2019-05-15 09:20:38 +0100

    Passes redis password from local env to kue's redis connection url
    Signed-off-by: beelaw <lawrence.mcbhee@gmail.com>

--- config/queue.js
+++ config/queue.js
@@ -2,7 +2,7 @@ const kue = require('kue');
 
 
 const queue = kue.createQueue({
-  redis: process.env.REDIS_URL,
+  redis: `${process.env.REDIS_URL}?${process.env.REDIS_PASS && `password=${process.env.REDIS_PASS}`}`,
 });
 
 queue.watchStuckJobs();
