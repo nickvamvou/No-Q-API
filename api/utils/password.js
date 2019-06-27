f2296934c759e53969cb226d0168f2448b463cbf
vamvou <nik.vamvou@gmail.com>
2019-06-27 20:26:49 +0400

    changed bcrypt to bcryptjs

--- api/utils/password.js
+++ api/utils/password.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const to = require("await-to-js").default;
 
 exports.checkPassCorrectness = async (plainTextPass, hashedPass) => {
